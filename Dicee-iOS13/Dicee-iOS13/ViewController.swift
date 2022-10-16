e2256f130f3b102b916eb0c9de33c34e5fa06924
deepvikas300 <115916966+deepvikas300@users.noreply.github.com>
2022-10-16 12:28:33 +0530

    Add files via upload

---
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //IBOutlet Allows Me to Refer to a UI Element
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //WHO           . WHAT = VALUE
+        diceImageViwOne.image = #imageLiteral(resourceName: "DiceSix")
+        //diceImageViewOne.alpha = 0.5
+        
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
+        
+    }
+
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageViewTwo.imge = #imageLiteral(resourceName: "DiceFour")
+    }
+}
+
